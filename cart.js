335b9948acea3329360ae4543dfcfbf23c10d9de
Zak Liles <zak.liles@gmail.com>
2021-12-06 20:24:17 -0700

    Completed Cart Problems

---
+++ cart.js
@@ -0,0 +1,101 @@
+///////////////////////////////////////////////
+///////////////////CART.JS/////////////////////
+///////////////////////////////////////////////
+/*
+    In this file, you'll be writing code to
+    calculate order totals. You'll also be 
+    creating customer objects.  
+*/
+
+
+//////////////////PROBLEM 1////////////////////
+/*  
+    Below is a cart array that has food objects
+    inside. 
+
+    Write a callback below that uses the reduce
+    array method to calculate the sum of all
+    the food. 
+*/
+
+const cart = [
+    {
+        name: 'pizza', 
+        price: 9.99
+    }, 
+    {
+        name: 'pasta', 
+        price: 8.99
+    }, 
+    {
+        name: 'salad', 
+        price: 7.99
+    }
+]
+
+//CODE HERE
+
+const summedPrice = cart.reduce((totalPrice, food) => totalPrice + food.price,0)
+// console.log(summedPrice)
+
+//////////////////PROBLEM 2////////////////////
+/*  
+    Write a function called `calcFinalPrice` that
+    can take in `cartTotal`,`couponValue`,
+    and `tax` arguments. 
+
+    Inside the function, calculate the tax 
+    on the cartTotal and add it in. Subtract
+    the value of the coupon. Return the final
+    number. 
+
+    Note: the numbers passed in for `tax` will be
+    decimals, for example: .06 for a 6% tax.
+*/
+
+//CODE HERE
+const calcFinalPrice = (cartTotal, couponValue, tax) => Math.round((cartTotal*(1+tax)-couponValue)*100)/100
+// console.log(calcFinalPrice(20,5,.07))
+
+//////////////////PROBLEM 3////////////////////
+/*  
+    In this problem, you'll create a model for 
+    a customer object as well as an example
+    object. 
+
+    Plan out a customer object for the cart page.
+    Think about the information that a 
+    restaurant would need about its customers.
+
+    In the TEXT ANSWER area below, describe the
+    properties that your customer object will have
+    and why you chose those properties.
+
+    Explain what data types each property should be
+    and why you chose those data types. 
+
+    Your object should have at least 4 properties. 
+*/
+
+/*
+    TEXT ANSWER HERE
+    Customer Object with following properties
+    name - string (sequence of characters, mostly letters)
+    address - string (allows house numbers such as 9 1/2 or 201A)
+    phone - string (while its comprised of numbers there is no need to perform math with phone numbers and should only be used exactly as is)
+    email - string (sequence of characters, mostly letters)
+
+*/
+
+/*
+    Now, create a customer object following your own
+    guidelines.
+*/
+
+//CODE HERE
+const johnDoe = {
+    name: "John Doe",
+    address: "123 Main Street",
+    phone: "(555) 555-5555",
+    email: "john.doe@email.com"
+}
